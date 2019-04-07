a93a21a3ce425cee776fa30eeca2a04386ceb674
Mingbo Peng <mingo1214@gmail.com>
2019-04-07 14:59:34 -0400

    Added OpenStudioCSharpApi Test

---
+++ src/OpenStudioCSharpApi.Test/OpsApi.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenStudio;
+
+namespace OpenStudioCSharpApi.Test
+{
+    [TestClass]
+    public class OpsApi
+    {
+        [TestMethod()]
+        public void CoilHeatingElecTest()
+        {
+
+            var m1 = new Model();
+
+            //1. Add heating coil before the fan
+            var lp1 = new AirLoopHVAC(m1);
+            var supplyOutletNode1 = lp1.supplyOutletNode();
+            ////Add heating coil
+            var coil1 = new CoilHeatingElectric(m1);
+            coil1.addToNode(supplyOutletNode1);
+            ////Add fan
+            var fan1 = new FanConstantVolume(m1);
+            fan1.addToNode(supplyOutletNode1);
+            ////Add SPM
+            var spm1 = new SetpointManagerScheduled(m1, new ScheduleRuleset(m1, 20));
+            spm1.addToNode(supplyOutletNode1);
+
+
+            //2. Add fan before heating coil
+            var lp2 = new AirLoopHVAC(m1);
+            var supplyOutletNode2 = lp2.supplyOutletNode();
+            ////Add fan
+            var fan2 = new FanConstantVolume(m1);
+            fan2.addToNode(supplyOutletNode2);
+            ////Add heating coil
+            var fanInletNode = fan2.inletModelObject().get().to_Node().get();
+            var coil2 = new CoilHeatingElectric(m1);
+            coil2.addToNode(fanInletNode);
+
+            ////Add SPM
+            var spm2 = new SetpointManagerScheduled(m1, new ScheduleRuleset(m1, 20));
+            spm2.addToNode(supplyOutletNode2);
+
+
+
+            //check temperatureSetpointNode
+            var success = true;
+            if (coil1.temperatureSetpointNode().is_initialized())
+            {
+                var coilTempNodeName = coil1.temperatureSetpointNode().get().nameString();
+                var SpmNodeName = supplyOutletNode1.nameString();
+                var isCoilTempNodeSetToSPM = coilTempNodeName == SpmNodeName;
+                //isCoilTempNodeSetToSPM is True;
+                success &= isCoilTempNodeSetToSPM == true;
+            }
+
+            if (coil2.temperatureSetpointNode().is_initialized())
+            {
+                var coilTempNodeName = coil2.temperatureSetpointNode().get().nameString();
+                var SpmNodeName = supplyOutletNode2.nameString();
+                var isCoilTempNodeSetToSPM = coilTempNodeName == SpmNodeName;
+                //isCoilTempNodeSetToSPM is False;
+                success &= isCoilTempNodeSetToSPM == false;
+            }
+
+            Assert.IsTrue(success);
+
+        }
+    }
+}
---
+++ src/OpenStudioCSharpApi.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenStudioCSharpApi.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenStudioCSharpApi.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a4d2697-16a2-4dc7-9e1c-603c91537d90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
